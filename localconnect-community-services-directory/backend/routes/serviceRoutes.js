d1c1fd5ff8ac38e83d75efa75f7d76f1cbc6b5ce
Nabil <md.nabil.khan@g.bracu.ac.bd>
2025-07-09 21:54:42 +0600

    db connected

--- localconnect-community-services-directory/backend/routes/serviceRoutes.js
+++ localconnect-community-services-directory/backend/routes/serviceRoutes.js
@@ -2,12 +2,11 @@ const express = require('express');
 const ServiceController = require('../controllers/serviceController');
 
 const router = express.Router();
-const serviceController = new ServiceController();
 
 function setRoutes(app) {
-    router.get('/services', serviceController.getAllServices.bind(serviceController));
-    router.post('/services', serviceController.createService.bind(serviceController));
-    router.post('/services/:id/book', serviceController.bookService.bind(serviceController));
+    router.get('/services', ServiceController.getAllServices);
+    router.post('/services', ServiceController.createService);
+    router.post('/services/:id/book', ServiceController.bookService);
 
     app.use('/api', router);
 }
